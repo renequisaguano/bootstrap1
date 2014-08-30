37a50e3f08b9bbe4d6354e1fef6697c73c465a40
Rene Quisaguano <renequisaguano@gmail.com>
2014-08-30 17:10:54 -0500

    Trabajando con grid, anidados y fluid

--- index.php
+++ index.php
@@ -4,17 +4,170 @@
 <meta charset="utf-8">
 	<title>Primer Proyecto Bootstrap</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+
+	<style type="text/css">
+
+	/*Desde un escritorio*/
+	@media(min-width: 1200px){
+
+
+
+
+	}
+
+
+
+	</style>
+
 </head>
 <body>
+
 <div class="container">
-		<h1>Esta es una prueba</h1>
-		<p>
-		Por primera vez, en la historia del Campus Party – Ecuador, el Área de Ciencia contará con un reto diseñado por la Universidad Regional Amazónica – IKIAM. 
+	<div class="row">
+		<div class="span12">
+			<h1>Esta es una prueba</h1>
+			<p>
+			Por primera vez, en la historia del Campus Party – Ecuador,
+			 el Área de Ciencia contará con un reto diseñado por la Universidad 
+			 Regional Amazónica – IKIAM. 
+			 El desafío consiste en desarrollar una aplicación para dispositivos 
+			 móviles que permita capturar y subir fotografías con datos y ubicación
+			 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+			 de crear una enciclopedia fotográfica y un mapa digital. 
+			</p>
+		</div>
+	</div>
 
-		El desafío consiste en desarrollar una aplicación para dispositivos móviles que permita capturar y subir fotografías con datos y ubicación geográfica de especies de la flora y la fauna de Ecuador con el objetivo de crear una enciclopedia fotográfica y un mapa digital. 
+	<div class="row">
+		<div class="span6">
+			<div class="row">
+				<div class="span3">
+
+					<h1>Esta es una prueba</h1>
+					<p>
+					Por primera vez, en la historia del Campus Party – Ecuador,
+					 el Área de Ciencia contará con un reto diseñado por la Universidad 
+					 Regional Amazónica – IKIAM. 
+					 El desafío consiste en desarrollar una aplicación para dispositivos 
+					 móviles que permita capturar y subir fotografías con datos y ubicación
+					 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+					 de crear una enciclopedia fotográfica y un mapa digital. 
+					</p>
+				</div>
+				<div class="span3">
+
+					<h1>Esta es una prueba</h1>
+					<p>
+					Por primera vez, en la historia del Campus Party – Ecuador,
+					 el Área de Ciencia contará con un reto diseñado por la Universidad 
+					 Regional Amazónica – IKIAM. 
+					 El desafío consiste en desarrollar una aplicación para dispositivos 
+					 móviles que permita capturar y subir fotografías con datos y ubicación
+					 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+					 de crear una enciclopedia fotográfica y un mapa digital. 
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="span6">
+			<h1>Esta es una prueba</h1>
+			<p>
+			Por primera vez, en la historia del Campus Party – Ecuador,
+			 el Área de Ciencia contará con un reto diseñado por la Universidad 
+			 Regional Amazónica – IKIAM. 
+			 El desafío consiste en desarrollar una aplicación para dispositivos 
+			 móviles que permita capturar y subir fotografías con datos y ubicación
+			 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+			 de crear una enciclopedia fotográfica y un mapa digital. 
+			</p>
+		</div>
+	</div>
+
+
+
+	<div class="row-fluid">
+		<div class="span3">
+			<div class="row-fluid">
+				<div class="span6">
+
+					<h1>Esta es una prueba</h1>
+					<p>
+					Por primera vez, en la historia del Campus Party – Ecuador,
+					 el Área de Ciencia contará con un reto diseñado por la Universidad 
+					 Regional Amazónica – IKIAM. 
+					 El desafío consiste en desarrollar una aplicación para dispositivos 
+					 móviles que permita capturar y subir fotografías con datos y ubicación
+					 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+					 de crear una enciclopedia fotográfica y un mapa digital. 
+					</p>
+				</div>
+
+
 			
+				<div class="span6">
 
-		</p>
+					<h1>Esta es una prueba</h1>
+					<p>
+					Por primera vez, en la historia del Campus Party – Ecuador,
+					 el Área de Ciencia contará con un reto diseñado por la Universidad 
+					 Regional Amazónica – IKIAM. 
+					 El desafío consiste en desarrollar una aplicación para dispositivos 
+					 móviles que permita capturar y subir fotografías con datos y ubicación
+					 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+					 de crear una enciclopedia fotográfica y un mapa digital. 
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="span3">
+			<h1>Esta es una prueba</h1>
+			<p>
+			Por primera vez, en la historia del Campus Party – Ecuador,
+			 el Área de Ciencia contará con un reto diseñado por la Universidad 
+			 Regional Amazónica – IKIAM. 
+			 El desafío consiste en desarrollar una aplicación para dispositivos 
+			 móviles que permita capturar y subir fotografías con datos y ubicación
+			 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+			 de crear una enciclopedia fotográfica y un mapa digital. 
+			</p>
+		</div>
+
+		<div class="span3">
+			<h1>Esta es una prueba</h1>
+			<p>
+			Por primera vez, en la historia del Campus Party – Ecuador,
+			 el Área de Ciencia contará con un reto diseñado por la Universidad 
+			 Regional Amazónica – IKIAM. 
+			 El desafío consiste en desarrollar una aplicación para dispositivos 
+			 móviles que permita capturar y subir fotografías con datos y ubicación
+			 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+			 de crear una enciclopedia fotográfica y un mapa digital. 
+			</p>
+		</div>
+
+		<div class="span3">
+			<h1>Esta es una prueba</h1>
+			<p>
+			Por primera vez, en la historia del Campus Party – Ecuador,
+			 el Área de Ciencia contará con un reto diseñado por la Universidad 
+			 Regional Amazónica – IKIAM. 
+			 El desafío consiste en desarrollar una aplicación para dispositivos 
+			 móviles que permita capturar y subir fotografías con datos y ubicación
+			 geográfica de especies de la flora y la fauna de Ecuador con el objetivo
+			 de crear una enciclopedia fotográfica y un mapa digital. 
+			</p>
+		</div>
+
+	</div>
+
+	
+
+
+	
 </div>
 
 </body>
