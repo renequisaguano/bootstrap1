b0ef89b145dce42b2bb26b876b46dee849f55368
Rene Quisaguano <renequisaguano@gmail.com>
2014-08-30 16:37:06 -0500

    archivos base

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Primer Proyecto Bootstrap</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+		<h1>Esta es una prueba</h1>
+		<p>
+		Por primera vez, en la historia del Campus Party – Ecuador, el Área de Ciencia contará con un reto diseñado por la Universidad Regional Amazónica – IKIAM. 
+
+		El desafío consiste en desarrollar una aplicación para dispositivos móviles que permita capturar y subir fotografías con datos y ubicación geográfica de especies de la flora y la fauna de Ecuador con el objetivo de crear una enciclopedia fotográfica y un mapa digital. 
+			
+
+		</p>
+</div>
+
+</body>
+</html>
