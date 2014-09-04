ea853ea0672b875bc72d3fc1c7b5bc17809fbedc
Rene Quisaguano <renequisaguano@gmail.com>
2014-09-03 20:09:08 -0500

    Estilos para los botones

--- index.php
+++ index.php
@@ -66,6 +66,87 @@
 
 
 <div class="row-fluid">
+	<div class="span4">
+		<a href="" class="btn">Link</a>
+		<input type="submit"  value="Submit" class="btn">
+		<input type="button" value="Boton" class="btn">
+		<button class="btn">Etiqueta</button>
+
+			<hr>
+		<a href="" class="btn-link">Link</a>
+		<input type="submit"  value="Submit" class="btn-link">
+		<input type="button" value="Boton" class="btn-link">
+		<button class="btn-link">Etiqueta</button>
+	</div>
+	<div class="span4">
+		<a href="" class="btn btn-primary">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-primary">
+		<input type="button" value="Boton" class="btn btn-primary">
+		<button class="btn btn-primary">Etiqueta</button>
+
+
+		<hr>
+		<a href="" class="btn btn-success">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-success">
+		<input type="button" value="Boton" class="btn btn-success">
+		<button class="btn btn-success">Etiqueta</button>
+
+
+
+
+		<hr>
+		<a href="" class="btn btn-warning">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-warning">
+		<input type="button" value="Boton" class="btn btn-warning">
+		<button class="btn btn-warning">Etiqueta</button>
+
+		<hr>
+		<a href="" class="btn btn-info">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-info">
+		<input type="button" value="Boton" class="btn btn-info">
+		<button class="btn btn-info">Etiqueta</button>
+
+		<hr>
+		<a href="" class="btn btn-danger">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-danger">
+		<input type="button" value="Boton" class="btn btn-danger">
+		<button class="btn btn-danger">Etiqueta</button>
+
+		<hr>
+		<a href="" class="btn btn-inverse">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-inverse">
+		<input type="button" value="Boton" class="btn btn-inverse">
+		<button class="btn btn-inverse">Etiqueta</button>
+
+
+	
+
+	</div>
+	<div class="span4">
+			
+		<a href="" class="btn  btn-small ">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-small">
+		<input type="button" value="Boton" class="btn btn-small">
+		<button class="btn btn-small">Etiqueta</button>
+
+		<hr>
+		<a href="" class="btn  btn-mini ">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-mini">
+		<input type="button" value="Boton" class="btn btn-mini">
+		<button class="btn btn-mini">Etiqueta</button>
+
+		<hr>
+		<a href="" class="btn  btn-block ">Link</a>
+		<input type="submit"  value="Submit" class="btn btn-block">
+		<input type="button" value="Boton" class="btn btn-block">
+		<button class="btn btn-block">Etiqueta</button>
+
+
+
+	</div>
+</div>
+
+<div class="row-fluid">
 	<div class="span12">
 		<table class="table table-bordered table-striped table-hover table-condensed">
 			<tr>
@@ -96,6 +177,58 @@
 
 	</div>
 </div>
+
+<div class="row-fluid">
+	<div class="span6">
+		<form action="">
+		<legend>Datos Personales</legend>
+			<label for="">Name</label>		
+			<input type="text">
+		<legend>Datos Facturacion</legend>
+			<label for="">Name</label>		
+			<input type="text">
+			<label for="">Name</label>		
+			<input type="text">
+			<br>
+			<input type="submit" class="btn">
+		</form>
+	</div>
+	<div class="span6">
+	<input type="text" class="search-query">
+	<hr>
+	<form action="" class="form-inline">
+	<input type="text">
+	<input type="submit" class="btn">
+	</form>
+	<form action="">
+		<div class="input-prepend">
+		<span class="add-on">@</span>
+		<input type="text">
+		</div>
+		<div class="input-append">
+		<input type="text">
+		<span class="add-on">@</span>
+		</div>
+
+		<div class="input-append input-prepend">
+		<span class="add-on">$</span>
+		<input type="text">
+		<span class="add-on">&</span>
+		</div>
+
+		<div class="input-append">
+		<input type="text">
+		<input type="submit" class="btn">
+		</div>
+
+
+	</form>
+		
+
+
+	</div>
+</div>
+
 
 
 
